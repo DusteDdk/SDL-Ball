d6de2153c160fd79458f7df970fed8d5890a8b4d
Jimmy Christensen <dusted@dusted.dk>
2016-08-25 18:21:38 +0200

    Added contigrator build script.

---
+++ contigrator-buildscript.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# This file is executed by contigratord when a build is triggered.
+# You can execute it manually too by executing /home/contigrator/contigrator/tool/trigBuild.sh
+
+#These variables are available:
+# $WD = The workspace $BN = Zero padded build Number (%07i) and $BN_NUM for unpadded.
+# $OUT = This empty directory is created for you to store the output of the build.
+set -e;set -x
+
+#Add your build instructions below:
+
+echo "Wizznic build script."
+
+make -f Makefile.win clean
+make -f Makefile clean
+
+
+OUTDIR="SDL-Ball_1.03_build-$BN_NUM"
+
+WINPKG="$OUTDIR"_windows.zip
+LINPKG="$OUTDIR"_linux.tar.bz2
+SRCPKG="$OUTDIR"_src.tar.xz
+
+mkdir -p $OUTDIR
+
+# Create the changelog
+git log > changelog.txt
+cp changelog.txt $OUTDIR
+
+# Compile for linux
+make -f Makefile
+mv sdl-ball $OUTDIR/sdl-ball_linux_x64
+make -f Makefile clean
+
+# Compile for windows
+make -f Makefile.win
+
+# Package datafiles
+cp -a leveleditor $OUTDIR
+cp -a themes $OUTDIR
+cp *.txt $OUTDIR
+
+# Package linux version
+
+tar -c $OUTDIR | pbzip2 > $LINPKG
+rm $OUTDIR/sdl-ball_linux_x64
+
+# Package windows version
+mv sdl-ball.exe $OUTDIR/SDL-Ball_windows.exe
+cp win32/readme_SDL.txt $OUTDIR
+unix2dos $OUTDIR/*.txt
+cp win32/*.dll $OUTDIR
+zip -r $WINPKG $OUTDIR
+make -f Makefile.win clean
+
+# Build the source code
+git archive --prefix="SDL-Ball_source_build_$BN"_src/ HEAD > "$OUTDIR"_src.tar
+tar --file "$OUTDIR"_src.tar --append --transform 's%^%SDL-Ball_source_build_'$BN'_src/%' changelog.txt
+cat "$OUTDIR"_src.tar | pxz > "$OUT/$SRCPKG"
+
+rm -R $OUTDIR *.tar
+rm changelog.txt
+
+mv $WINPKG $OUT
+mv $LINPKG $OUT
+
