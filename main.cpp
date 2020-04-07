b5194a16f5465281669a847af5b047e58054cf19
DusteD <dusted@dusted.dk>
2020-04-07 12:25:32 +0200

    Fix last 2 compiler warnings.

--- main.cpp
+++ main.cpp
@@ -3758,7 +3758,7 @@ int main (int argc, char *argv[]) {
 
   difficulty = static_difficulty;
 
-  cout << "SDL-Ball v "VERSION << endl;
+  cout << "SDL-Ball v " VERSION << endl;
 
   // default to "" (If this have a 0 len after trying to getenv, it defaults to ./)
   privFile.programRoot = ""; 
--- menu.cpp
+++ menu.cpp
@@ -204,7 +204,7 @@ public:
         switch(t)
         {
           case 0:
-          sprintf(highScores[t], "SDL-BALL v "VERSION );
+          sprintf(highScores[t], "SDL-BALL v " VERSION );
           break;
           case 1:
           sprintf(highScores[t], "-----------------------");
